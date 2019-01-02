5bc39fb84ad94be42310b42e6bf68699b7e300c2
magda.zelezik <magda,zelezik@gmail.com>
2019-01-02 14:24:03 +0100

    installed angular

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-np;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const config = require("./config/database");
+const path = require("path");
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  config.uri,
+  err => {
+    if (err) console.log("There is an error " + err);
+    else console.log("Connected to db");
+  }
+);
+app.use(express.static(__dirname + "/client/dist/"));
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/client/dist/index.html"));
+});
+
+app.listen(8080, () => {
+  console.log("listening on 8080");
+});
